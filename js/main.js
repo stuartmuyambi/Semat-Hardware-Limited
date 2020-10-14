aa84bfa9f882c8c572ef7a7c3f4524be04b43fb7
Stuart Muyambi <stuartmuyambi@gmail.com>
2020-10-14 09:53:37 +0300

    Remove slider next and prev buttons

--- js/main.js
+++ js/main.js
@@ -12,5 +12,5 @@ function showSlides() {
         slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000);
+    setTimeout(showSlides, 3000);
 }
